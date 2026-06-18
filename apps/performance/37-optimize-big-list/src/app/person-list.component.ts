064ae86e97e4c9cc2fa1819358318d466148ae13
Betty Spurgeon <betty@salonkee.lu>
2026-06-18 16:14:01 +0200

    feat(challenge37): optimize big list solution

--- apps/performance/37-optimize-big-list/src/app/person-list.component.ts
+++ apps/performance/37-optimize-big-list/src/app/person-list.component.ts
@@ -1,19 +1,22 @@
+import { ScrollingModule } from '@angular/cdk/scrolling';
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { Person } from './person.model';
 
 @Component({
   selector: 'app-person-list',
+  imports: [ScrollingModule],
   template: `
-    <div class="relative h-[300px] overflow-hidden">
-      <div class="absolute inset-0 overflow-scroll">
-        @for (person of persons(); track person.email) {
-          <div class="flex h-9 items-center justify-between border-b">
-            <h3>{{ person.name }}</h3>
-            <p>{{ person.email }}</p>
-          </div>
-        }
+    <cdk-virtual-scroll-viewport
+      appendOnly
+      itemSize="36"
+      class="h-[300px] border-2 border-black">
+      <div
+        *cdkVirtualFor="let person of persons()"
+        class="flex h-9 items-center justify-between border-b">
+        <h3>{{ person.name }}</h3>
+        <p>{{ person.email }}</p>
       </div>
-    </div>
+    </cdk-virtual-scroll-viewport>
   `,
   host: {
     class: 'w-full flex flex-col',
